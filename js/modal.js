19909d94c285f7eed7ad2e4edcd7a0d204af84e3
rakib2067 <rakib7@hotmail.co.uk>
2022-03-28 13:42:03 +0100

    modal close working

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,6 @@ let modalClose = document.querySelector(".modal-close");
 let modalBg = document.querySelector(".modal-bg");
 modalBtn.addEventListener("click", toggleModal);
 modalClose.addEventListener("click", toggleModal);
-modalBg.addEventListener("click", toggleModal);
 
 function toggleModal() {
   document.querySelector(".modal-bg").classList.toggle("bg-active");
