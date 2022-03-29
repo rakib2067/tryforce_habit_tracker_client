9891ff3b1cf81b3be9168752a22f0dc39e6d9f46
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-29 03:12:36 +0100

    remove gif profile pictures. can select static gif images

--- js/modal.js
+++ js/modal.js
@@ -12,25 +12,46 @@ function toggleModal() {
   document.querySelector(".modal-bg").classList.toggle("bg-active");
 }
 
+//code graveyard :(
+//function getTrending() {
+//  toggleModal();
+//  fetch(
+//    `https://api.giphy.com/v1/gifs/search?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&q=zelda&limit=25&offset=0&rating=g&lang=en`
+//  )
+//    .then((res) => {
+//      return res.json();
+//    })
+//    .then(({ data }) => {
+//      data.forEach((gif) => {
+//        let image = document.createElement("img");
+//        image.src = gif.images["fixed_height_small"].url;
+//        image.classList.add("gif-image");
+//        document.querySelector(".modal--content").append(image);
+//      });
+//    })
+//    .catch((error) => {
+//      alert("Error: ", error);
+//    });
+//}
+
 function getTrending() {
+  document.querySelector(".modal--content").innerHTML = '';
   toggleModal();
-  fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=8PiyixOfCPFFExgTLW5347Y8xbuMoYGk&q=zelda&limit=25&offset=0&rating=g&lang=en`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then(({ data }) => {
-      data.forEach((gif) => {
-        let image = document.createElement("img");
-        image.src = gif.images["fixed_height_small"].url;
-        image.classList.add("gif-image");
-        document.querySelector(".modal--content").append(image);
-      });
-    })
-    .catch((error) => {
-      alert("Error: ", error);
+  const data = ["/images/link.png", "/images/link2.png", "/images/zelda.png"];
+  for (let i = 0; i < data.length; i++) {
+    let image = document.createElement("img");
+    image.src = data[i];
+    image.classList.add("gif-image");
+    image.id = i;
+    image.addEventListener('click', e => {
+      const children = document.querySelector(".modal--content").children;
+      for (let item of children) {
+        item.classList.remove("image--selected");
+      }
+      e.target.classList.add("image--selected");
     });
+    document.querySelector(".modal--content").append(image);
+  }
 }
 
 if (imageCont) {
